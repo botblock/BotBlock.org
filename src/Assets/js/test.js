7c0016f20373eec842d9a87e64d311e8ef43840a
MattIPv4 <matthew@cowley.org.uk>
2019-07-14 17:12:56 +0100

    Add timer to the web tests

--- src/Assets/js/test.js
+++ src/Assets/js/test.js
@@ -1,5 +1,6 @@
 var interval;
 var previous;
+var start;
 var btn = document.getElementById('run');
 var code = document.getElementById('tests');
 var state = document.getElementById('state');
@@ -9,10 +10,18 @@ function runTests() {
     btn.setAttribute('disabled', true);
     btn.classList.add('disabled');
     state.textContent = 'Initialising';
+    start = Date.now();
     var index = 0;
     var spinner = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
     interval = setInterval(function () {
-        state.textContent = '\nRunning tests ' + spinner[index];
+        var since = Date.now() - start;
+        var d = new Date(since);
+        var h = d.getUTCHours();
+        var m = d.getUTCMinutes();
+        var s = d.getUTCSeconds();
+        var ms = d.getUTCMilliseconds();
+        var time = (h ? h + 'h:' : '') + (m ? m + 'm:' : '') + s + 's.' + ms + 'ms';
+        state.textContent = '\nRunning tests (' + time + ') ' + spinner[index];
         index++;
         if (index >= spinner.length) index = 0;
     }, 50);
@@ -21,9 +30,9 @@ function runTests() {
 
 function fetchOutput() {
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            if (xhr.responseText.trim() === "end") {
+            if (xhr.responseText.trim() === 'end') {
                 clearInterval(interval);
                 state.textContent = '';
                 btn.removeAttribute('disabled');
