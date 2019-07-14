13d3bda1f171c0abe73f4beb72ec7bc35a9ff6b6
MattIPv4 <matthew@cowley.org.uk>
2019-07-14 12:41:45 +0100

    Dont use env port for tests

--- tests/base.js
+++ tests/base.js
@@ -7,7 +7,7 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-const target = `${config.baseURL}:${process.env.PORT || config.port}`;
+const target = `${config.baseURL}:${config.port}`;
 const request = () => chai.request(target);
 const ratelimitBypass = (req) => req.set('X-Ratelimit-Bypass', config.secret);
 
