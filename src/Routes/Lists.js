25e5e2ea3fe4b6a26f25233ae351f11f3945472e
MattIPv4 <matthew@cowley.org.uk>
2019-07-15 21:36:58 +0100

    Add hidden lists page

--- src/Routes/Lists.js
+++ src/Routes/Lists.js
@@ -11,8 +11,8 @@ class ListsRoute extends BaseRoute {
 
     footerData() {
         return new Promise((resolve, reject) => {
-            this.db.run('SELECT discord_only FROM lists WHERE defunct = ? AND display = ?', [0, 1]).then((active) => {
-                this.db.run('SELECT COUNT(id) as count FROM lists WHERE defunct = ? AND display = ?', [1, 1]).then((defunct) => {
+            this.db.run('SELECT discord_only FROM lists WHERE defunct = 0 AND display = 1').then((active) => {
+                this.db.run('SELECT COUNT(id) as count FROM lists WHERE defunct = 1 AND display = 1').then((defunct) => {
                     const discordOnly = active.filter(list => list.discord_only);
                     resolve({active: active.length, discordOnly: discordOnly.length, defunct: defunct[0].count});
                 }).catch(reject);
@@ -23,7 +23,7 @@ class ListsRoute extends BaseRoute {
     routes() {
         this.router.get('/', (req, res) => {
             try {
-                this.db.run('SELECT * FROM lists WHERE defunct = ? AND display = ? ORDER BY discord_only DESC, LOWER(name) ASC', [0, 1]).then((lists) => {
+                this.db.run('SELECT * FROM lists WHERE defunct = 0 AND display = 1 ORDER BY discord_only DESC, LOWER(name) ASC').then((lists) => {
                     this.footerData().then((footer) => {
                         res.render('lists/lists', {title: 'All Bot Lists', lists, footer});
                     });
@@ -35,7 +35,7 @@ class ListsRoute extends BaseRoute {
 
         this.router.get('/new', (req, res) => {
             try {
-                this.db.run('SELECT * FROM lists WHERE defunct = ? AND display = ? ORDER BY added DESC LIMIT 4', [0, 1]).then((lists) => {
+                this.db.run('SELECT * FROM lists WHERE defunct = 0 AND display = 1 ORDER BY added DESC LIMIT 4').then((lists) => {
                     this.footerData().then((footer) => {
                         res.render('lists/lists', {
                             title: 'New Bot Lists',
@@ -51,11 +51,27 @@ class ListsRoute extends BaseRoute {
 
         this.router.get('/defunct', (req, res) => {
             try {
-                this.db.run('SELECT * FROM lists WHERE defunct = ? ORDER BY discord_only DESC, LOWER(name) ASC', [1, 1]).then((lists) => {
+                this.db.run('SELECT * FROM lists WHERE defunct = 1 ORDER BY discord_only DESC, LOWER(name) ASC').then((lists) => {
                     this.footerData().then((footer) => {
                         res.render('lists/lists', {
                             title: 'Defunct Bot Lists',
                             subtitle: `These lists are flagged as defunct on ${res.__('site_name')}`,
+                            lists, footer
+                        });
+                    });
+                });
+            } catch {
+                res.status(500).render('error', {title: 'Database Error'});
+            }
+        });
+
+        this.router.get('/hidden', (req, res) => {
+            try {
+                this.db.run('SELECT * FROM lists WHERE defunct = 0 AND display = 0 ORDER BY discord_only DESC, LOWER(name) ASC').then((lists) => {
+                    this.footerData().then((footer) => {
+                        res.render('lists/lists', {
+                            title: 'Hidden Bot Lists',
+                            subtitle: `These lists are currently hidden on ${res.__('site_name')}.\nTo find out more about why each list here is hidden, press the Information button on the list card.`,
                             lists, footer
                         });
                     });
@@ -85,7 +101,7 @@ class ListsRoute extends BaseRoute {
                 this.db.run('SELECT * FROM features WHERE id = ? LIMIT 1', [req.params.id]).then((features) => {
                     if (!features.length) return res.status(404).render('error', {
                         title: 'Page not found',
-                        status: 400,
+                        status: 404,
                         message: 'The page you were looking for could not be found.'
                     });
                     this.db.run('SELECT * FROM lists LEFT OUTER JOIN (SELECT * FROM feature_map WHERE feature_map.feature = ?) temp ON temp.list = lists.id WHERE display = 1 AND defunct = 0 AND temp.feature IS NOT NULL ORDER BY discord_only DESC, LOWER(name) ASC', [req.params.id]).then((lists) => {
@@ -103,20 +119,25 @@ class ListsRoute extends BaseRoute {
         });
 
         this.router.get('/:id', (req, res) => {
-            this.db.run('SELECT * FROM lists WHERE id = ? LIMIT 1', [req.params.id]).then((lists) => {
-                if (!lists.length) return res.status(404).render('error', {
-                    title: 'Page not found',
-                    status: 400,
-                    message: 'The page you were looking for could not be found.'
+            try {
+                this.db.run('SELECT * FROM lists WHERE id = ? LIMIT 1', [req.params.id]).then((lists) => {
+                    if (!lists.length) return res.status(404).render('error', {
+                        title: 'Page not found',
+                        status: 404,
+                        message: 'The page you were looking for could not be found.'
+                    });
+                    this.db.run('SELECT features.name as name, IFNULL(temp.value, 0) as value, features.display as display, features.type as type, features.id as id FROM features LEFT OUTER JOIN (SELECT * FROM feature_map WHERE feature_map.list = ?) temp ON temp.feature = features.id ORDER BY temp.value DESC, features.display DESC, features.name ASC', [lists[0].id]).then((features) => {
+                        res.render('lists/list', {
+                            title: 'All Lists',
+                            list: lists[0],
+                            checkboxes: features,
+                            hideUncheckedBoxes: true
+                        });
+                    });
                 });
-                this.db.run('SELECT features.name as name, IFNULL(temp.value, 0) as value, features.display as display, features.type as type, features.id as id FROM features LEFT OUTER JOIN (SELECT * FROM feature_map WHERE feature_map.list = ?) temp ON temp.feature = features.id ORDER BY temp.value DESC, features.display DESC, features.name ASC', [lists[0].id]).then((features) => {
-                    res.render('lists/list', {title: 'All Lists', list: lists[0], checkboxes: features, hideUncheckedBoxes: true });
-                }).catch((e) => {
-                    res.status(500).render('error', {title: 'Database Error'});
-                })
-            }).catch(() => {
+            } catch {
                 res.status(500).render('error', {title: 'Database Error'});
-            })
+            }
         });
     }
 
