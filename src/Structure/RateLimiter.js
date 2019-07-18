8834b83e3604af1a9e0910b5cd3ced81cf780cae
MattIPv4 <matthew@cowley.org.uk>
2019-07-18 10:09:43 +0100

    Fix ratelimiting issues

--- src/Structure/RateLimiter.js
+++ src/Structure/RateLimiter.js
@@ -6,22 +6,24 @@ class RateLimiter {
     checkRatelimit(requestLimit = 1, timeLimit = 1, bot = '') {
         return async (req, res, next) => {
             if (req.body.bot_id && !bot) bot = req.body.bot_id;
+            bot = bot.toString();
             try {
                 await this.db.run('DELETE FROM ratelimit WHERE ip = ? AND bot_id = ? AND route = ? AND expiry < ?', [req.ip, bot, req.originalUrl, Date.now()]);
                 const recent = await this.db.run('SELECT * FROM ratelimit WHERE ip = ? AND bot_id = ? AND route = ? ORDER BY datetime DESC', [req.ip, bot, req.originalUrl]);
                 if (recent && recent.length >= requestLimit) {
-                    const lastRequest = recent[0].datetime;
                     const expiry = recent[0].expiry;
-                    res.set('Retry-After', new Date(expiry).getSeconds() - new Date().getSeconds());
-                    res.set('X-Rate-Limit-Reset', expiry);
+                    const retry = Math.round((expiry - Date.now()) / 1000);
+                    const reset = Math.round(expiry / 1000);
+                    res.set('Retry-After', retry);
+                    res.set('X-Rate-Limit-Reset', reset);
                     res.set('X-Rate-Limit-IP', req.ip);
                     res.set('X-Rate-Limit-Route', req.originalUrl);
                     res.set('X-Rate-Limit-Bot-ID', bot);
                     return res.status(429).json({
                         error: true,
                         status: 429,
-                        retry_after: new Date(expiry).getSeconds() - new Date().getSeconds(),
-                        ratelimit_reset: expiry,
+                        retry_after: retry,
+                        ratelimit_reset: reset,
                         ratelimit_ip: req.ip,
                         ratelimit_route: req.originalUrl,
                         ratelimit_bot_id: bot
