6887ad3f001270bd0491644d8541727864e796de
Jacob Rayner <jacob.rayner@outlook.com>
2019-07-29 12:15:14 +0100

    Started Cache system

---
+++ src/Structure/Cache.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+class Cache {
+    constructor(file) {
+        this.file = file;
+    }
+
+    add(data) {
+
+    }
+
+    delete() {
+
+    }
+
+    getAll() {
+
+    }
+}
+
+module.exports = Cache;
