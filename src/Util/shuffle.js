c7ae6c395822138501c21f6096fd0449193b84c7
MattIPv4 <me@mattcowley.co.uk>
2019-10-05 15:06:33 +0100

    Use new DB in Routes/Index

---
+++ src/Util/shuffle.js
@@ -0,0 +1,20 @@
+// Credit: https://github.com/Daplie/knuth-shuffle/blob/master/index.js
+module.exports = array => {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
