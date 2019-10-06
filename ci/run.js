f6080c0d2023d96ba1fbe5977504359975b6d5a0
MattIPv4 <me@mattcowley.co.uk>
2019-10-06 18:46:14 +0100

    Add full CI script

--- ci/run.js
+++ ci/run.js
@@ -1,13 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+const test = require('../deploy/src/test');
+
 const main = async () => {
     // Detect if there is an existing config
+    const configLocation = path.join(__dirname, '..', 'config.js');
+    let configExists = false, configBackupLocation;
+    if (fs.existsSync(configLocation)) {
+        configExists = true;
+        configBackupLocation = path.join(__dirname, '..', 'config.backup.js');
+        fs.renameSync(configLocation, configBackupLocation);
+    }
 
     // Copy CI config to root
+    const configCILocation = path.join(__dirname, 'config.ci.js');
+    fs.copyFileSync(configCILocation, configLocation);
 
     // Run tests
+    let out, err;
+    out = await test().catch(e => {
+        err = e;
+    });
 
     // Restore original config
+    fs.unlinkSync(configLocation);
+    if (configExists) fs.renameSync(configBackupLocation, configLocation);
 
-    // Throw/return tests result
+    // Throw/log tests result
+    if (err) throw err;
+    console.log(out);
 };
 
 main();
