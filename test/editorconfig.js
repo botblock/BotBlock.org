fbfa2df3a1940d1cfbbcdea728d80b664b2c9955
MattIPv4 <me@mattcowley.co.uk>
2019-10-09 11:43:31 +0100

    Clean up mocha test

--- test/editorconfig.js
+++ test/editorconfig.js
@@ -1,13 +1,13 @@
-const { describe, it, expect, secret } = require('./base');
+const { describe, it } = require('./base');
 const path = require('path');
 const exec = require('child_process').exec;
 
 describe('editorconfig', () => {
-    it('should follow editorconfig conventions', done => {
-        exec('npm run lint:editorconfig', { cwd: path.join(__dirname, '..') }, (err, stdout, stderr) => {
-            if (err) {
-                return done(new Error(stdout));
-            }
+    it('should follow editorconfig conventions', function (done) {
+        this.retries(0);
+        this.slow(2000);
+        exec('npm run lint:editorconfig', { cwd: path.join(__dirname, '..') }, (err, stdout) => {
+            if (err) return done(new Error(stdout));
             done();
         });
     });
