2021b1cbb6becba748e4e9399362820953b1fd6a
Vivek Kumar Singh <vsviveksingh30@gmail.com>
2019-10-09 16:47:47 +0530

    Add eslint config & test (#25)
    * added eslint
    
    * Added eslint for mocha and changed to string rule
    
    * ESLint mocha test
    
    * Update ruleset
    
    * Update ruleset

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "mocha": true,
+        "es6": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-bitwise": "error",
+        "eqeqeq": "error",
+        "no-eq-null": "error",
+        "wrap-iife": [
+            "error",
+            "any"
+        ],
+        "no-use-before-define": [
+            "error",
+            {
+                "functions": false
+            }
+        ],
+        "new-cap": "error",
+        "no-new": "error",
+        "no-caller": "error",
+        "no-undef": "error",
+        "max-len": [
+            "error",
+            {
+                "code": 200,
+                "ignoreComments": true
+            }
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-const-assign": "error",
+        "quotes": [
+            "error",
+            "single",
+            {
+                "avoidEscape": true
+            }
+        ],
+        "quote-props": [
+            "error",
+            "as-needed",
+            {
+                "numbers": true
+            }
+        ],
+        "no-console": "off",
+        "no-cond-assign": "off",
+    }
+};
