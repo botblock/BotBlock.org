d6a0f2507c174746d5efef15cb069e68355fd4ec
Don Chea <donaldducky@gmail.com>
2019-10-09 06:52:01 -0400

    Add .editorconfig (#24)
    * Add .editorconfig
    
    fixes #22
    
    * Fix editorconfig violations
    
    Ignore db seeds as it is auto-generated.
    
    * Ignore entire data directory
    
    * Run editorconfig check as a mocha test
    
    * Clean up mocha test

---
+++ test/editorconfig.js
@@ -0,0 +1,14 @@
+const { describe, it } = require('./base');
+const path = require('path');
+const exec = require('child_process').exec;
+
+describe('editorconfig', () => {
+    it('should follow editorconfig conventions', function (done) {
+        this.retries(0);
+        this.slow(2000);
+        exec('npm run lint:editorconfig', { cwd: path.join(__dirname, '..') }, (err, stdout) => {
+            if (err) return done(new Error(stdout));
+            done();
+        });
+    });
+});
