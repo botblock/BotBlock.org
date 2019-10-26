246a875b9841b255548925874b534778d055db82
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2019-10-26 19:03:02 +0100

    Fix tests in deployed environments (#31)
    * Skip if exec fails in code style tests
    
    * Try logging the full error?
    
    * Rely on error code?

--- test/code-style.js
+++ test/code-style.js
@@ -2,23 +2,32 @@ const { describe, it } = require('./base');
 const path = require('path');
 const exec = require('child_process').exec;
 
-describe('editorconfig', () => {
+
+describe('code style', () => {
     it('should follow editorconfig conventions', function (done) {
         this.retries(0);
         this.slow(2000);
         exec('npm run lint:editorconfig', { cwd: path.join(__dirname, '..') }, (err, stdout) => {
-            if (err) return done(new Error(stdout));
+            if (err) {
+                // Check failed
+                if (err.code === 1) return done(new Error(stdout));
+                // Command failed
+                else return this.skip();
+            }
             done();
         });
     });
-});
 
-describe('eslint', () => {
     it('should follow eslint standards', function (done) {
         this.retries(0);
         this.slow(2000);
         exec('npm run lint:eslint', { cwd: path.join(__dirname, '..') }, (err, stdout) => {
-            if (err) return done(new Error(stdout));
+            if (err) {
+                // Check failed
+                if (err.code === 1) return done(new Error(stdout));
+                // Command failed
+                else return this.skip();
+            }
             done();
         });
     });
