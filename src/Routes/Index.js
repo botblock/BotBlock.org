91958f6791a1c653901d56c67663492c833f995e
Jacob <22778299+DetectiveHuman@users.noreply.github.com>
2019-12-31 13:12:21 +0000

    About section manager (#54)
    * Started about manager
    
    * Add ability to add, edit & remove about sections
    
    * Remove useless 'Up' & 'Down' buttons.
    
    * Remove pointless 'is-right' class.
    
    * Change returning error page to redirect.
    
    * Remove pointless css.
    
    * Add about manager to sidebar
    
    * Pad codemirror & remove buggy line numbers
    
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- src/Routes/Index.js
+++ src/Routes/Index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { join } = require('path');
 const BaseRoute = require('../Structure/BaseRoute');
+const FormValidator = require('../Structure/FormValidator');
 const Renderer = require('../Structure/Markdown');
 const shuffle = require('../Util/shuffle');
 const handleError = require('../Util/handleError');
@@ -42,6 +43,96 @@ class IndexRoute extends BaseRoute {
                 handleError(this.db, req, res, e.stack);
             });
         });
+
+        this.router.get('/about/manage', this.requiresAuth.bind(this), this.isAdmin.bind(this), (req, res) => {
+            this.db.select().from('about').orderBy('position', 'asc').then((data) => {
+                const sections = data.map((section) => {
+                    section.title = this.renderer.variables(section.title);
+                    section.content = this.renderer.render(section.content);
+                    return section;
+                });
+                res.render('about/manage', { title: 'About', sections });
+            }).catch((e) => {
+                handleError(this.db, req, res, e.stack);
+            });
+        });
+
+        this.router.get('/about/manage/add', this.requiresAuth.bind(this), this.isAdmin.bind(this), (req, res) => {
+            res.render('about/edit', { title: 'Add Section', data: {} });
+        });
+
+        this.router.post('/about/manage/add', this.requiresAuth.bind(this), this.isAdmin.bind(this), async (req, res) => {
+            try {
+                let changes = {};
+                const validate = FormValidator.validateAboutSection(req.body);
+                if (validate && validate.length > 0) return res.render('about/edit', { title: 'Add Section', data: req.body, errors: validate });
+                const columns = Object.keys(await this.db('about').columnInfo());
+                for (const column of columns) {
+                    if (req.body[column]) {
+                        changes[column] = req.body[column];
+                    } else {
+                        changes[column] = null;
+                    }
+                }
+                await this.db('about').insert(changes);
+                res.redirect('/about/manage');
+            } catch (e) {
+                handleError(this.db, req, res, e.stack);
+            }
+        });
+
+        this.router.get('/about/manage/:id', this.requiresAuth.bind(this), this.isAdmin.bind(this), (req, res) => {
+            this.db.select().from('about').where({ id: req.params.id }).then((data) => {
+                if (!data.length) return res.status(404).render('error', {
+                    title: 'Page not found',
+                    status: 404,
+                    message: 'The page you were looking for could not be found.'
+                });
+                res.render('about/edit', { title: 'Edit Section', data: data[0] });
+            }).catch((e) => {
+                handleError(this.db, req, res, e.stack);
+            });
+        });
+
+        this.router.post('/about/manage/:id', this.requiresAuth.bind(this), this.isAdmin.bind(this), (req, res) => {
+            this.db.select().from('about').where({ id: req.params.id }).then(async(data) => {
+                if (!data.length) return res.status(404).render('error', {
+                    title: 'Page not found',
+                    status: 404,
+                    message: 'The page you were looking for could not be found.'
+                });
+                let changes = {};
+                const validate = FormValidator.validateAboutSection(req.body);
+                if (validate && validate.length > 0) return res.render('about/edit', { title: 'Edit Section', data: req.body, errors: validate });
+                const columns = Object.keys(await this.db('about').columnInfo());
+                for (const column of columns) {
+                    if (req.body[column]) {
+                        changes[column] = req.body[column];
+                    } else {
+                        changes[column] = null;
+                    }
+                }
+                await this.db('about').where({ id: req.body.id }).update(changes);
+                res.redirect('/about/manage');
+            }).catch((e) => {
+                handleError(this.db, req, res, e.stack);
+            });
+        });
+
+        this.router.get('/about/manage/:id/delete', this.requiresAuth.bind(this), this.isAdmin.bind(this), (req, res) => {
+            this.db.select().from('about').where({ id: req.params.id }).then(async(data) => {
+                if (!data.length) return res.status(404).render('error', {
+                    title: 'Page not found',
+                    status: 404,
+                    message: 'The page you were looking for could not be found.'
+                });
+                await this.db('about').where({ id: req.params.id }).del();
+                res.redirect('/about/manage');
+            }).catch((e) => {
+                handleError(this.db, req, res, e.stack);
+            });
+        });
+
 
         this.router.get('/sitemap', (req, res) => {
             sitemap.get(this.db).then(data => {
