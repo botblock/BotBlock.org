674ca6b6b87a3c76d134c504a360185e246b6cf0
MattIPv4 <me@mattcowley.co.uk>
2020-03-23 18:07:14 +0000

    Try no password in CI config

--- ci/config.ci.js
+++ ci/config.ci.js
@@ -25,7 +25,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'root',
+        password: '',
         database: 'botblock',
         client: 'mysql'
     },
